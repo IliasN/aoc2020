a7da9a2a6de141886684664225b5d6fc6afd4940
ilias.nhairi <ilias.nhairi@etu.hesge.ch>
2020-12-03 18:51:09 +0100

    Day 3

---
+++ Day3/main.py
@@ -0,0 +1,35 @@
+lines = []
+with open("data", "r") as f:
+    line = f.readline()
+    while line:
+        lines.append(line)
+        line = f.readline()
+WIDTH = 31 # Attention aux newlines
+HEIGHT = len(lines)
+
+arr = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
+
+for i in range(HEIGHT):
+    for j in range(WIDTH):
+        arr[i][j] = lines[i][j]
+
+def trees_from_slope(right, down):
+    x = 0
+    count = 0
+    for y in range(0, HEIGHT, down):
+        c = arr[y][x]
+        if c == "#":
+            count += 1
+        x = (x + right) % WIDTH
+    return count
+
+# PARTIE 1
+print(f"Part one : {trees_from_slope(3,1)}")
+# PARTIE 2
+result = 1
+result *= trees_from_slope(1,1)
+result *= trees_from_slope(3,1)
+result *= trees_from_slope(5,1)
+result *= trees_from_slope(7,1)
+result *= trees_from_slope(1,2)
+print(f"Part two : {result}")
